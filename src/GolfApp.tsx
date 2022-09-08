225d962e338141420404acf4cf73fb9e3a88767a
Paul Thornton <p.d.thornton995@gmail.com>
2022-09-08 12:33:11 +1200

    blankScorecard added and main app page built out

--- src/GolfApp.tsx
+++ src/GolfApp.tsx
@@ -1,12 +1,72 @@
 import React from 'react'
-import { initialCourse, initialPlayers } from './initialValues'
+import { initialCourse, initialPlayers, scorecard } from './initialValues'
+
+import { Typography, Paper, AppBar, Toolbar, Grid, ListItem, Divider, Card, CardHeader, CardContent } from '@mui/material';
+
 
 function GolfApp() {
+    
     return(
-        <>
-        <h1>GOLF APP!</h1>
-        {/* try mapping through my holes and printing our all the details here */}
-        </>
+        <Paper
+            style={{
+                padding: 0,
+                margin: 0,
+                height: "100vh",
+                backgroundColor: "#fafafa",
+               
+            }}
+            elevation={0}
+        >
+            <AppBar color='secondary' position='static' style={{ height: "64px" }}>
+                <Toolbar>
+                    <Typography color='inherit'>SHANKERS</Typography>
+                </Toolbar>
+            </AppBar>
+
+        
+            <Grid container style={{ 
+                marginTop: "1rem",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: 'flex-start',
+                alignItems: "center",
+            }}>
+                <Grid item xs={11} md={8} lg={4} style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: 'flex-start',
+                    alignItems: "stretch",
+                    width: '500px'
+                }}>
+
+                    <Card style={{
+                        marginBottom: '1rem', 
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: 'flex-start',
+                        alignItems: "center" 
+                        }}>
+                        <CardHeader><h2>Hole 1</h2></CardHeader>
+                        <CardContent>Stroke Index: 1</CardContent>
+                        <CardContent>Par 3</CardContent>
+                        <CardContent>245m</CardContent>
+                    </Card>
+
+                    {/* these need to be compponents */}
+                    <Paper style={{marginBottom: '1rem'}}>
+                    <ListItem style={{ height: "64px" }}>{initialPlayers[0]}</ListItem>
+                    <Divider />
+                    <ListItem style={{ height: "64px" }}>Shots: </ListItem>
+                    </Paper>
+
+                    <Paper>
+                    <ListItem style={{ height: "64px" }}>{initialPlayers[1]}</ListItem>
+                    <Divider />
+                    <ListItem style={{ height: "64px" }}>Shots: </ListItem>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Paper>
     )
 }
 
--- src/initialValues.ts
+++ src/initialValues.ts
@@ -3,56 +3,32 @@ export const initialCourse = {
     location: 'Pauatahanui, Porirua',
     numHoles: 9,
     holes: [
-        {hole1: {
-                par: 4,
-                strokeIndex: 7,
-                tee: 258,
-        }},
-        {hole2: {
-            par: 4,
-            strokeIndex: 1,
-            tee: 359,
-        }},
-        {hole3: {
-            par: 3,
-            strokeIndex: 4,
-            tee: 182,
-        }},
-        {hole4: {
-            par: 4,
-            strokeIndex: 9,
-            tee: 276,
-        }},
-        {hole5: {
-            par: 4,
-            strokeIndex: 2,
-            tee: 388,
-        }},
-        {hole6: {
-            par: 3,
-            strokeIndex: 8,
-            tee: 147,
-        }},
-        {hole7: {
-            par: 5,
-            strokeIndex: 6,
-            tee: 446,
-        }},
-        {hole8: {
-            par: 5,
-            strokeIndex: 3,
-            tee: 384,
-        }},
-        {hole9: {
-            par: 4,
-            strokeIndex: 5,
-            tee: 199,
-        }}
+        { name: 'Hole 1', par: 4, strokeIndex: 7, tee: 258 },
+        { name: 'Hole 2', par: 4, strokeIndex: 1, tee: 359 },
+        { name: 'Hole 3', par: 3, strokeIndex: 4, tee: 182 },
+        { name: 'Hole 4', par: 4, strokeIndex: 9, tee: 276 },
+        { name: 'Hole 5', par: 4, strokeIndex: 2, tee: 388 },
+        { name: 'Hole 6', par: 3, strokeIndex: 8, tee: 147 },
+        { name: 'Hole 7', par: 5, strokeIndex: 6, tee: 446 },
+        { name: 'Hole 8', par: 5, strokeIndex: 3, tee: 384 },
+        { name: 'Hole 9', par: 4, strokeIndex: 5, tee: 199,}
     ]
 }
 
 
 export const initialPlayers = [
-    {p1: 'Pauly T'},
-    {p2: 'Marty Bull'}
+    'Pauly T',
+    'Marty Bull'
+]
+
+export const scorecard = [
+    {p1Score: undefined, p2Score: undefined},
+    {p1Score: undefined, p2Score: undefined},
+    {p1Score: undefined, p2Score: undefined},
+    {p1Score: undefined, p2Score: undefined},
+    {p1Score: undefined, p2Score: undefined},
+    {p1Score: undefined, p2Score: undefined},
+    {p1Score: undefined, p2Score: undefined},
+    {p1Score: undefined, p2Score: undefined},
+    {p1Score: undefined, p2Score: undefined},
 ]
